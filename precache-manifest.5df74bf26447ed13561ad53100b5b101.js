361915719a5940f15765093ba16722badcb130c0
Seibeaaar <yuribar4520@gmail.com>
2020-04-19 21:43:27 +0300

    Updates

---
+++ precache-manifest.5df74bf26447ed13561ad53100b5b101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "875a773833a7b22322862331ee88e8fe",
+    "url": "/COVID19-Tracker/index.html"
+  },
+  {
+    "revision": "201761801d273705236f",
+    "url": "/COVID19-Tracker/static/css/main.02527f9a.chunk.css"
+  },
+  {
+    "revision": "254b08b45d696c1ecdfd",
+    "url": "/COVID19-Tracker/static/js/2.7cd1c7fe.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID19-Tracker/static/js/2.7cd1c7fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201761801d273705236f",
+    "url": "/COVID19-Tracker/static/js/main.8a8f127b.chunk.js"
+  },
+  {
+    "revision": "f57f5a938305aaddc3c3",
+    "url": "/COVID19-Tracker/static/js/runtime-main.4e75cd07.js"
+  }
+]);
